799922d022f1bf5bd20433d5b6f9b07e2f88cb5e
lukas enoch chengo <lukas.enoch.chengo29@gmail.com>
2025-11-23 19:15:56 +0300

    yes

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++){
+        const message = (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        console.log(message);
+        messages.push(message);
+    } 
+    return messages;   
+}
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "birthday";
+writeCards(names, event); 
+function countDown(){
+    let i = 10;
+    while(i >= 0){ 
+        console.log(i);
+        --i;
+    }
+}
+countDown();
